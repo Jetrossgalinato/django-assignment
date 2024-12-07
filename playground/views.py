0b58d1b7bb3e4df02c697505de152f6a76bdc103
Jetross <jetrossneri07@gmail.com>
2024-12-07 15:30:59 +0800

    feat: finished setting up django

--- playground/views.py
+++ playground/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def calculate():
+    # Your calculation logic here
+    x = 1
+    y = 2
+    return x
+
+def say_hello(request):
+    x = calculate()
+    return render(request, 'hello.html')
